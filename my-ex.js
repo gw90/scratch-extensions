e1c4646582c89f6bc9c16a345160c2cec175d497
gw90 <gw90@users.noreply.github.com>
2015-05-09 12:07:18 -0400

    Update my-ex.js

--- my-ex.js
+++ my-ex.js
@@ -10,7 +10,7 @@
 
     ext.my_first_block = function() {
         // Code that gets executed when the block is run
-        return "hi";
+        return Math.floor(Math.random()*11);
     };
 
     // Block and block menu descriptions
