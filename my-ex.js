daf36dafd3e725621ed93d836155794219dec0a0
gw90 <gw90@users.noreply.github.com>
2015-05-15 15:51:58 -0400

    Update my-ex.js

--- my-ex.js
+++ my-ex.js
@@ -28,13 +28,7 @@
         return uno % dos;
     };
     
-    ext.pi = function() {
-        return 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679;
-    };
-    
-    ext.sub_pi = function(min, max) {
-        return "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679".substring(min-1,max+1);
-    };
+
 
     // Block and block menu descriptions
     var descriptor = {
@@ -45,8 +39,7 @@
             ['r', 'negative infinity', 'neg_inf'],
             ['b', '%n is finite?', 'inf_b'],
             ['r', 'remainder of %n divided by %n', 'mod'],
-            ['r', 'pi', 'pi'],
-            ['r', 'digits %n to %n of pi', 'sub_pi', 1, 10]
+            
         ]
     };
 
