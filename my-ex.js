fa6d974d22c37b2eafaead5c7868535579f81d79
gw90 <gw90@users.noreply.github.com>
2015-05-15 13:21:56 -0400

    Update my-ex.js

--- my-ex.js
+++ my-ex.js
@@ -25,7 +25,7 @@
     };
     
     ext.mod = function(uno, dos) {
-        return uno % dos;
+        return 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679;
     };
 
     // Block and block menu descriptions
