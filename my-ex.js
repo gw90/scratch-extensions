d1d77ad98824e068fdb7590b1a9da068ffeec12f
gw90 <gw90@users.noreply.github.com>
2015-05-25 12:18:50 -0400

    Update my-ex.js

--- my-ex.js
+++ my-ex.js
@@ -70,7 +70,7 @@
         blocks: [
             // Block type, block name, function name
             ['r', '%n ^ %n', 'power', 2, 3],
-            ['r', 'infinity', 'inf', 'http://gw90.github.io/scratch-extension/'],
+            ['r', 'infinity', 'inf'],
             ['r', 'negative infinity', 'neg_inf', ],
             ['b', '%n is finite?', 'inf_b'],
             ['r', 'remainder of %n divided by %n', 'mod'],
